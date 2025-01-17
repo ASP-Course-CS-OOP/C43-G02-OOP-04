9951d8e40721ba47fe97df46f1d05ce8564c7a99
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-17 18:39:26 +0200

    Part 01 Polymorphism

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 Polymorphism
+
+            //Done [Check NoteBad].
+
+            #endregion
         }
     }
 }
