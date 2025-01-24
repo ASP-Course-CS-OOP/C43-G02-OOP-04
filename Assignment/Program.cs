2c057b334c2da2325d0d0e3360e918db296a7a64
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-24 14:45:00 +0200

    Part01 [Updated]

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -7,57 +7,57 @@
 
             #region Part01
 
-            #region Q01 - What is the primary purpose of an interface in C#?
+            #region Q1 - What is the primary purpose of an interface in C#?
 
-            //a) To provide a way to implement multiple inheritance
+            // a) To provide a way to implement multiple inheritance
 
             #endregion
 
-            #region Q02 - Which of the following is NOT a valid access modifier for interface members in C#?
+            #region Q2 - Which of the following is NOT a valid access modifier for interface members in C#?
 
             // a) private
 
             #endregion
 
-            #region Q03 - Can an interface contain fields in C#?
+            #region Q3 - Can an interface contain fields in C#?
 
-            // b) No
+            // c) Only if they are static
 
             #endregion
 
-            #region Q04 - In C#, can an interface inherit from another interface?
+            #region Q4 - In C#, can an interface inherit from another interface?
 
             // b) Yes, interfaces can inherit from multiple interfaces
 
             #endregion
 
-            #region Q05 - Which keyword is used to implement an interface in a class in C#?
+            #region Q5 - Which keyword is used to implement an interface in a class in C#?
 
             // d) implements
 
             #endregion
 
-            #region Q06 - Can an interface contain static methods in C#?
+            #region Q6 - Can an interface contain static methods in C#?
 
             // a) Yes
 
             #endregion
 
-            #region Q07 - In C#, can an interface have explicit access modifiers for its members?
+            #region Q7 - In C#, can an interface have explicit access modifiers for its members?
 
             // b) No, all members are implicitly public
 
             #endregion
 
-            #region Q08 - What is the purpose of an explicit interface implementation in C#?
+            #region Q8 - What is the purpose of an explicit interface implementation in C#?
 
             // b) To provide a clear separation between interface and class members
 
             #endregion
 
-            #region Q09 - In C#, can an interface have a constructor?
+            #region Q9 - In C#, can an interface have a constructor?
 
-            // b) No, interfaces cannot have constructors
+            //b) No, interfaces cannot have constructors
 
             #endregion
 
@@ -110,14 +110,14 @@
 
             #region Q03 - we define the INotificationService interface with a method SendNotification that takes a recipient and a message as parameters.
 
-                //INotificationService emailService = new EmailNotificationService();
-                //emailService.SendNotification("eslam.saadany22@gmail.com", "Welcome to the platform!");
+            //INotificationService emailService = new EmailNotificationService();
+            //emailService.SendNotification("eslam.saadany22@gmail.com", "Welcome to the platform!");
 
-                //INotificationService smsService = new SmsNotificationService();
-                //smsService.SendNotification("01022010887", "Your OTP is 123456.");
+            //INotificationService smsService = new SmsNotificationService();
+            //smsService.SendNotification("01022010887", "Your OTP is 123456.");
 
-                //INotificationService pushService = new PushNotificationService();
-                //pushService.SendNotification("Iphone 8 Plus", "You have a new message.");
+            //INotificationService pushService = new PushNotificationService();
+            //pushService.SendNotification("Iphone 8 Plus", "You have a new message.");
 
             #endregion
 
